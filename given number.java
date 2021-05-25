428c56798b1cac34ced53362d84fdfa6b1fbf9e3
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-05-26 01:28:11 +0600

    Create given number.java

---
+++ given number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Baby");
+		Scanner input = new Scanner(System.in);
+		int number;
+		number=input.nextInt();
+		System.out.println("the number is = "+number);
+		
+	}
+}
