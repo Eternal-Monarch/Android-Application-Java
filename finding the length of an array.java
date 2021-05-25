76272df3883a67f771dc4e787f8815f46c1dbc21
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-05-26 03:09:02 +0600

    Create finding the length of an array.java

---
+++ finding the length of an array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{ 
+	public static void main(String[] args) 
+	{
+	    int [] number = new int[5];
+	    int [] shuvo = new int [2];
+	    number[0]=10;
+	    number[1]=20;
+	    number[2]=30;
+	    number[3]=40;
+	    number[4]=50;
+	    
+	    int len = number.length;
+	    System.out.println(len);
+	    
+	}
+        
+    }
+    
+		
