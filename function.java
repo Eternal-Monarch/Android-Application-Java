706fbcb0b7a6726e349513421b0a5084576cfae8
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-05-25 21:59:35 +0600

    Create function.java

---
+++ function.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String args[]) 
+	{
+		int result=square(12);
+		System.out.println(result);
+	}
+	//secondary fucntion
+	static int square(int nubmer)
+	{
+	    int square =number*number;
+	    return square;
+	}
+}
